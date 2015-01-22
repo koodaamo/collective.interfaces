86a0c2062dae3ad4fbdceb84de32ca65ecf54fa5
Petri Savolainen <petri.savolainen@koodaamo.fi>
2015-01-22 14:39:59 +0200

    set correct domain in message factory

--- collective/interfaces/__init__.py
+++ collective/interfaces/__init__.py
@@ -1,7 +1,7 @@
 from plone.theme.interfaces import IDefaultPloneLayer
 from zope.i18nmessageid import MessageFactory
 
-_ = MessageFactory("collective.blogging")
+_ = MessageFactory("collective.interfaces")
 
 class IInterfacesSpecific(IDefaultPloneLayer):
     """ A marker interface that defines a Zope 3 browser layer. """
